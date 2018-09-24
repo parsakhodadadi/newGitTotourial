d329aa85665a6f5dae4c17212cb3efb15e2be8cf
Parsa Khodadadi <parsakhodadadi2003@gmail.com>
2018-09-24 09:04:35 -0400

    Initial Commit

---
+++ newGitTotourial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newGitTotourial
+//
+//  Created by Parsa Khodadadi on 24/09/2018.
+//  Copyright © 2018 Parsa Khodadadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
